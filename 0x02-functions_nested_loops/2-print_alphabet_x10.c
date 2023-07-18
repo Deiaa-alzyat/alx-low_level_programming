0be1e330435e43467ffda8403db6b024ff16bf55
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2023-07-18 09:18:33 -0700

    updated

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,19 @@
-#include "holberton.h"
-
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabet letters
+* ten times
+*desc: The function uses loop to operate
+*Return: no return
+*/
 void print_alphabet_x10(void)
 {
-    char letter = 'a';
-    int i;
+int counter = 0;
+int letter;
+while (counter++ <= 9)
+{
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 
-    for (i = 0; i < 10; i++)
-    {
-        while (letter <= 'z')
-        {
-            _putchar(letter);
-            letter++;
-        }
-
-        letter = 'a'; // Reset letter to 'a' for the next iteration
-    }
-
-    _putchar('\n');
+_putchar(10);
+}
 }
